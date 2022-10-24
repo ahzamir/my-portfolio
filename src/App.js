5f9d1922d647d161d592b4e57876ac1733381408
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-10-25 02:07:52 +0430

    choose the main colors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="bg-red-500">Hello World</h1>
+        <h1>Hello World</h1>
+        <p>My name is Zamir</p>
+        <span>Yeah boy!</span>
       </header>
     </div>
   );
