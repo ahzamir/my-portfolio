462cc81d918386991d0461e334ca30cfe2dd7b1c
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-10-26 01:16:17 +0430

    add the header and humberger menu to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Navbar />
+  </div>
+);
 
 export default App;
