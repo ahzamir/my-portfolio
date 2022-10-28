6a501a4ee6becba5f7a089d4710c7ef0dc7a3b86
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-10-28 15:54:50 +0430

    created the About component

---
+++ src/components/pages/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import aboutSvg from '../../assets/svgs/about-page.svg';
+
+const About = () => (
+  <section id="about" className="row m-0 pt-5 pb-5 h-100">
+  </section>
+);
+
+export default About;
