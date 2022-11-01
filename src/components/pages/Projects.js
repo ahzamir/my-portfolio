94adf83030c324a41eb7f69218df0eaaf4285ac4
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-11-01 20:58:49 +0430

    add some style to the links on model and solved the linters issues

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Carousel from 'react-bootstrap/Carousel';
+import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
 import projectsSvg from '../../assets/svgs/projects-page.svg';
 
 const suiteUpImage = require('../../assets/project-screenshots/suite-up.png');
@@ -16,14 +17,14 @@ const bookStoreAppImage = require('../../assets/project-screenshots/book-store.p
 const mathMagiciansImage = require('../../assets/project-screenshots/math-magicians-1.png');
 // const mathMagiciansImage2 = require('../../assets/project-screenshots/math-magicians-mobile.svg');
 const topRatedMoviesImage = require('../../assets/project-screenshots/top-rated-movies.png');
-// const topRatedMoviesImage2 = require('../../assets/project-screenshots/top-rated-movies-1.png');
+const topRatedMoviesImage2 = require('../../assets/project-screenshots/top-rated-movies-1.png');
 const leaderboardImage = require('../../assets/project-screenshots/leaderboard-1.png');
 // const leaderboardImage2 = require('../../assets/project-screenshots/leaderboard-mobile.svg');
 const awesomeBooksImage = require('../../assets/project-screenshots/awesom-books-1.png');
-// const awesomeBooksImage2 = require('../../assets/project-screenshots/awesome-books-2.png');
+const awesomeBooksImage2 = require('../../assets/project-screenshots/awesom-books-2.png');
 // const awesomeBooksImage3 = require('../../assets/project-screenshots/awesome-book-mobile.svg');
 const incomingMovieImage = require('../../assets/project-screenshots/incoming-movies-1.png');
-// const incomingMovieImage2 = require('../../assets/project-screenshots/incoming-movies-2.png');
+const incomingMovieImage2 = require('../../assets/project-screenshots/incoming-movies-2.png');
 // const incomingMovieImage3 = require('../../assets/project-screenshots/incoming-movies-mobile.svg');
 
 const Projects = () => {
@@ -61,6 +62,12 @@ const Projects = () => {
       description: 'Budget App is a Ruby on Rails app which allows users to create a budget and track their expenses. It uses a PostgreSQL database to store the data, RESTful API to communicate with the database, Devise for user authentication, and Swagger for API documentation.',
       image: budgetAppImage,
       languages_and_tools: ['Ruby on Rails', 'PostgreSQL', 'Heroku', 'Swagger', 'Devise'],
+      more_images: [
+        budgetAppImage,
+        // budgetAppImage2,
+      ],
+      live_link: 'https://zamir-budget-app.herokuapp.com/users/sign_in',
+      source_link: 'https://github.com/ahzamir/budget-app',
     },
     {
       id: '3',
@@ -68,6 +75,12 @@ const Projects = () => {
       description: 'Weather App is a React app which allows users to search for a city and state and view the current weather conditions and a 5-day forecast. It uses the OpenWeatherMap API to get the weather data.',
       image: weatherAppImage,
       languages_and_tools: ['React', 'OpenWeatherMap API', 'Netlify', 'Bootstrap'],
+      more_images: [
+        weatherAppImage,
+        // weatherAppImage2,
+      ],
+      live_link: 'https://gregarious-pavlova-aaa751.netlify.app/',
+      source_link: 'https://github.com/ahzamir/weather-condition-app',
     },
     {
       id: '4',
@@ -75,6 +88,11 @@ const Projects = () => {
       description: 'Bookstore CMS is a React app which allows users to add, remove, and edit books. It uses a PostgreSQL database to store the data, RESTful API to communicate with the database, and React Router to handle routing.',
       image: bookStoreAppImage,
       languages_and_tools: ['React', 'PostgreSQL', 'React Router', 'Netlify', 'Bootstrap'],
+      more_images: [
+        bookStoreAppImage,
+      ],
+      live_link: 'https://62ba1732b6caef0aee728265--leafy-gingersnap-7d2f91.netlify.app/',
+      source_link: 'https://github.com/ahzamir/Book-Store',
     },
     {
       id: '5',
@@ -82,6 +100,12 @@ const Projects = () => {
       description: 'Math Magicians is a React app which allows users to perform basic math operations. It uses React Router to handle routing.',
       image: mathMagiciansImage,
       languages_and_tools: ['React', 'React Router', 'Netlify', 'Bootstrap'],
+      more_images: [
+        mathMagiciansImage,
+        // mathMagiciansImage2,
+      ],
+      live_link: 'https://62ab3f1807095e1efe36b2cd--quiet-gumdrop-092633.netlify.app/',
+      source_link: 'https://github.com/ahzamir/Math-Magicians',
     },
     {
       id: '6',
@@ -89,6 +113,12 @@ const Projects = () => {
       description: 'Top-Rated Movies is a JavaScript app which allows users to view the 20 top-rated movies. It uses the TMDB API to get the movie data.',
       image: topRatedMoviesImage,
       languages_and_tools: ['JavaScript', 'TMDB API', 'Netlify', 'Bootstrap'],
+      more_images: [
+        topRatedMoviesImage,
+        topRatedMoviesImage2,
+      ],
+      live_link: 'https://63472938939be20b59bda72c--stellular-cassata-c5fee6.netlify.app/',
+      source_link: 'https://github.com/felixodette/Javascript-Group-Capstone',
     },
     {
       id: '7',
@@ -96,6 +126,12 @@ const Projects = () => {
       description: 'Leaderboard is a JavaScript app which allows users to view the players scores and add their own score. It uses the Webpack and ES6 modules to organize the code, and Microverse API to get the player data.',
       image: leaderboardImage,
       languages_and_tools: ['JavaScript', 'Webpack', 'ES6 modules', 'Microverse API', 'Netlify', 'Bootstrap'],
+      more_images: [
+        leaderboardImage,
+        // leaderboardImage2,
+      ],
+      live_link: 'https://ahzamir.github.io/Leaderboard/',
+      source_link: 'https://github.com/ahzamir/Leaderboard',
     },
     {
       id: '8',
@@ -103,6 +139,12 @@ const Projects = () => {
       description: 'Awesome Books is a JavaScript app which allows users to add and remove books. It uses the ES6 modules to organize the code.',
       image: awesomeBooksImage,
       languages_and_tools: ['HTML', 'JavaScript', 'ES6 modules', 'GitHub Pages', 'CSS'],
+      more_images: [
+        awesomeBooksImage,
+        awesomeBooksImage2,
+      ],
+      live_link: 'https://ahzamir.github.io/Awessom-books-with-ES6/',
+      source_link: 'https://github.com/ahzamir/Awessom-books-with-ES6',
     },
     {
       id: '9',
@@ -110,6 +152,12 @@ const Projects = () => {
       description: 'Incoming Movies Event is a JavaScript app which allows users to view an upcoming event about movies. It shows the event details, the speakers, and the schedule.',
       image: incomingMovieImage,
       languages_and_tools: ['HTML', 'JavaScript', 'GitHub Pages', 'CSS'],
+      more_images: [
+        incomingMovieImage,
+        incomingMovieImage2,
+      ],
+      live_link: 'https://ahzamir.github.io/Incoming-Movies/',
+      source_link: 'https://github.com/ahzamir/Incoming-Movies',
     },
   ];
   const handleClick = (event) => {
@@ -195,7 +243,7 @@ const Projects = () => {
                   key={image}
                 >
                   <img
-                    className="img-fluid"
+                    className="img-fluid border border-2 border-white rounded shadow"
                     src={image}
                     alt={projectData.title}
                   />
@@ -228,6 +276,9 @@ const Projects = () => {
                     target="_blank"
                     rel="noreferrer"
                   >
+                    <i className="me-2">
+                      <FaExternalLinkAlt />
+                    </i>
                     Live Link
                   </a>
                   <a
@@ -236,6 +287,11 @@ const Projects = () => {
                     target="_blank"
                     rel="noreferrer"
                   >
+                    <i
+                      className="me-2"
+                    >
+                      <FaGithub />
+                    </i>
                     Source Link
                   </a>
                 </div>
