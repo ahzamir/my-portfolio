50acefc9f3bdbdd4596dff84e832127b3c0977b3
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-11-03 18:45:16 +0430

    added the recommendations data

---
+++ src/components/pages/recomendations/recomendations_data.js
@@ -0,0 +1,40 @@
+const felixOdette = require('../../../assets/recommendations_data/felix.png');
+const huseinBiyik = require('../../../assets/recommendations_data/husein.png');
+const emmanuelPaul = require('../../../assets/recommendations_data/emmanuel.png');
+const ahmadZakiYousufi = require('../../../assets/recommendations_data/zaki.png');
+const nedjwaBouraiou = require('../../../assets/recommendations_data/nedjwab.png');
+
+const recommendations = [
+  {
+    id: 1,
+    name: 'Felix Odette',
+    recommendation: 'Ahmad is great at finding software solutions and implementing workflow processes by assessing operational capability, troubleshooting and diagnostics, designing and calibrating system specifications, and analysing system processes. Ahmad\'s also excellent at staying up to date on software development and thus finding new solutions. He has extensive and diverse knowledge of software development best practices, and great at giving help and advice. He\'s reliable and highly capable, I can’t recommend him highly enough.',
+    image: felixOdette,
+  },
+  {
+    id: 2,
+    name: 'Husein Biyik',
+    recommendation: 'Ahmad is exactly the sort of developer any company would love. He has brilliant communication skills. I worked alongside him while building 3 different projects and he was always positive and open to learning from others. He was great at remote teamwork, and pair programming and has a keen eye for design. He is also super friendly and respectful so when we worked together, I felt like we\'d known each other for years. I can\'t recommend him enough!',
+    image: huseinBiyik,
+  },
+  {
+    id: 3,
+    name: 'Emmanuel Paul',
+    recommendation: 'Ahmad Zamir Yousufi is a good team player who is committed to the success of the team. He is a progressive software developer and connects very well with teammates and is willing to see things from different perspectives. I enjoyed working in the same group with him a number of times.',
+    image: emmanuelPaul,
+  },
+  {
+    id: 4,
+    name: 'Ahmad Zaki Yousufi',
+    recommendation: 'Ahmad Zamir Yousufi is an excellent developer with astonishing talent in competitive programming and is flexible with using different programming languages for different purposes. He has experience working as a Front-end developer. He is excellent and fast on any coding challenges and problem-solving. I am very happy having him as a Mentor and teammate on KICK-START Team and Microverse program and certainly can get my full recommendation.',
+    image: ahmadZakiYousufi,
+  },
+  {
+    id: 5,
+    name: 'Nedjwa Bouraiou',
+    recommendation: 'I had the chance to work with Ahmed on an interesting react-redux project, he is so professional and productive person. he is well organized, diligent, and a fast learner. Ahmed consistently delivers high-quality codes and meets or exceeds deadlines. I was amazed by his clean code and javascript knowledge. Anyone would be lucky to have him on his team.',
+    image: nedjwaBouraiou,
+  },
+];
+
+export default recommendations;
