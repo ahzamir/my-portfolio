a3e202f80217ee9c7bf1c04a23e57e4c82695e43
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-11-04 19:12:33 +0430

    added the contact page svg

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import contactSvg from '../../assets/svgs/contact-page.svg';
 
 const Contact = () => (
     <section id="contact" className="row m-0 mt-md-5 mb-md-5">
-        <div className="col-md-3 d-none d-md-flex justify-content-center align-items-center">
-            <img src={recomendationsSvg} alt="Home" className="img-fluid" />
-        </div>
-        <div className="col-md-9">
+        <div className="col-md-8">
             <div className="d-flex mb-4">
-                <h2 className="secondary-color mb-3 fw-bold fs-4">Recomendations</h2>
+                <h2 className="secondary-color mb-3 fw-bold fs-4">Contact Me</h2>
                 <hr className="ms-3 w-25" />
             </div>
             <div>
                 Contact
             </div>
+        </div>
+        <div className="col-md-4 d-none d-md-flex justify-content-center align-items-center">
+            <img src={contactSvg} alt="Home" className="img-fluid" />
         </div>
     </section>
 );
