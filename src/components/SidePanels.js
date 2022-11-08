78898d5511f6bd12a6fd91c228f39a8504966072
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-11-08 19:55:43 +0430

    add links to the side panels

--- src/components/SidePanels.js
+++ src/components/SidePanels.js
@@ -4,9 +4,15 @@ import { FaGithub, FaLinkedin, FaAngellist } from 'react-icons/fa';
 const SidePanels = () => (
   <div className="h-100">
     <ul className="h-50 d-flex flex-column justify-content-end align-items-center list-style-none">
-      <li><i className="fa fa-github"><FaGithub className="quaternary-color side-panels" /></i></li>
-      <li><i className="fa fa-linkedin"><FaLinkedin className="quaternary-color side-panels" /></i></li>
-      <li><i className="fa fa-angellist"><FaAngellist className="quaternary-color side-panels" /></i></li>
+      <a href="https://github.com/ahzamir" target="_blank" rel="noopener noreferrer">
+        <li><i className="fa fa-github"><FaGithub className="quaternary-color side-panels" /></i></li>
+      </a>
+      <a href="https://www.linkedin.com/in/ahzamir/" target="_blank" rel="noopener noreferrer">
+        <li><i className="fa fa-linkedin"><FaLinkedin className="quaternary-color side-panels" /></i></li>
+      </a>
+      <a href="https://angel.co/u/ahzamir" target="_blank" rel="noopener noreferrer">
+        <li><i className="fa fa-angellist"><FaAngellist className="quaternary-color side-panels" /></i></li>
+      </a>
     </ul>
     <hr className="d-none d-md-flex" id="hr" />
   </div>
