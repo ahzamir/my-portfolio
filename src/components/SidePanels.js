7e97c29c996b5eac1afdc41f1267ea20e8c6664a
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-11-08 18:49:24 +0430

    created the form fields with material ui and changes some designs for it

--- src/components/SidePanels.js
+++ src/components/SidePanels.js
@@ -4,9 +4,9 @@ import { FaGithub, FaLinkedin, FaAngellist } from 'react-icons/fa';
 const SidePanels = () => (
   <div className="h-100">
     <ul className="h-50 d-flex flex-column justify-content-end align-items-center list-style-none">
-      <li><i className="fa fa-github"><FaGithub /></i></li>
-      <li><i className="fa fa-linkedin"><FaLinkedin /></i></li>
-      <li><i className="fa fa-angellist"><FaAngellist /></i></li>
+      <li><i className="fa fa-github"><FaGithub className='quaternary-color side-panels' /></i></li>
+      <li><i className="fa fa-linkedin"><FaLinkedin className='quaternary-color side-panels' /></i></li>
+      <li><i className="fa fa-angellist"><FaAngellist className='quaternary-color side-panels' /></i></li>
     </ul>
     <hr className="d-none d-md-flex" id="hr" />
   </div>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,31 +1,100 @@
 import React from 'react';
 import contactSvg from '../../assets/svgs/contact-page.svg';
+import { TextField } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
-const Contact = () => (
-    <section id="contact" className="row m-0 mt-md-5 mb-md-5">
-        <div className="col-md-8">
-            <div className="d-flex mb-4">
-                <h2 className="secondary-color mb-3 fw-bold fs-4">Contact Me</h2>
-                <hr className="ms-3 w-25" />
+const Contact = () => {
+    const ValidationTextField = styled(TextField)({
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: 'var(--quaternary-color)',
+            },
+            '&:hover fieldset': {
+                borderColor: 'var(--tertiary-color)',
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: 'var(--secondary-color)',
+                boxShadow: '0 0 6px 0 var(--secondary-color)',
+            },
+        },
+        '& .MuiFormHelperText-root': {
+            color: 'var(--quaternary-color)',
+        },
+        '& input:valid + fieldset': {
+            borderColor: 'green',
+            borderWidth: 2,
+            boxShadow: '0 0 8px green',
+        },
+        '& input': {
+            color: 'var(--quaternary-color)',
+        },
+        '& label': {
+            color: 'var(--quaternary-color)',
+            opacity: '0.7',
+        },
+        '& label.Mui-focused': {
+            color: 'var(--secondary-color)',
+            opacity: '1',
+        },
+    });
+
+    return (
+        <section id="contact" className="row m-0 mt-md-5 mb-md-5">
+            <div className="col-md-8">
+                <div className="d-flex mb-4">
+                    <h2 className="secondary-color mb-3 fw-bold fs-4">Contact Me</h2>
+                    <hr className="ms-3 w-25" />
+                </div>
+                <div>
+                    <p className="tertiary-color mb-3 fw-bold fs-6">I'm always interested in hearing about new projects, so if you'd like to chat
+                        please get in touch.</p>
+                    <form action="https://formspree.io/f/xayvdjzb" method="post" autoComplete="off">
+                        <ValidationTextField
+                            label="Name"
+                            variant="outlined"
+                            className="mb-3"
+                            fullWidth
+                            required
+                            helperText="Please enter your name"
+                            name="name"
+                            type="text"
+                        />
+                        <ValidationTextField
+                            label="Email"
+                            variant="outlined"
+                            className="mb-3"
+                            fullWidth
+                            required
+                            helperText="Please enter your email"
+                            name="email"
+                            type="email"
+                        />
+                        <ValidationTextField
+                            label="Message"
+                            variant="outlined"
+                            className="mb-3"
+                            fullWidth
+                            required
+                            helperText="Please enter your message"
+                            multiline
+                            rows={4}
+                            name="message"
+                            type="text"
+                            InputProps={{
+                                style: {
+                                    color: 'var(--quaternary-color)',
+                                },
+                            }}
+                        />
+                        <button type="submit" value="send" className="btn bg-main border-white secondary-color mt-3">Submit</button>
+                    </form>
+                </div>
             </div>
-            <div>
-                <p className="tertiary-color mb-3 fw-bold fs-6">I'm always interested in hearing about new projects, so if you'd like to chat
-                    please get in touch.</p>
-                <form action="https://formspree.io/f/xayvdjzb" method="post">
-                    <label htmlFor="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="name" name="name" placeholder="Enter your name" required />
-                    <label htmlFor="email" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="email" name="email" placeholder="Enter your email" required />
-                    <label htmlFor="message" className="form-label">Message</label>
-                    <textarea className="form-control" id="message" rows="3" name="message" placeholder="Enter your message" required></textarea>
-                    <button type="submit" value="send" className="btn bg-main border-white secondary-color mt-3">Submit</button>
-                </form>
+            <div className="col-md-4 d-none d-md-flex justify-content-center align-items-center">
+                <img src={contactSvg} alt="Home" className="img-fluid" />
             </div>
-        </div>
-        <div className="col-md-4 d-none d-md-flex justify-content-center align-items-center">
-            <img src={contactSvg} alt="Home" className="img-fluid" />
-        </div>
-    </section >
-);
+        </section >
+    );
+}
 
 export default Contact;
